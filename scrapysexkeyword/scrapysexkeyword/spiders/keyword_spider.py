98c945e6bbb91fabc99444660a7d08676992b0a0
jianze_zeng <jianze_zeng@foxitsoftware.com>
2022-11-10 10:36:04 +0800

    update url in console

--- scrapysexkeyword/scrapysexkeyword/spiders/keyword_spider.py
+++ scrapysexkeyword/scrapysexkeyword/spiders/keyword_spider.py
@@ -1,7 +1,7 @@
 import scrapy
 import logging
 from scrapysexkeyword.scrapysexkeyword.items import ScrapysexkeywordItem
-from scrapysexkeyword.scrapysexkeyword.iso3166 import ISO3166
+# from scrapysexkeyword.scrapysexkeyword.iso3166 import ISO3166
 
 logger = logging.getLogger(__name__)
 class KeywordSpider(scrapy.Spider):
@@ -12,15 +12,15 @@ class KeywordSpider(scrapy.Spider):
         super().__init__(**kwargs)
     def start_requests(self):
         meta = {'REDIRECT_ENABLED':True}
-        urls = []
-        for x in ISO3166:
-            urls.append('https://www.xvideos.com/change-country/'+x.lower())     
-        logger.info(urls)
-        # urls = [
-        #     'https://www.xvideos.com/change-country/au',
-        #     'https://www.xvideos.com/change-country/us',
-        #     'https://www.xvideos.com/change-country/at',       
-        # ]
+        # urls = []
+        # for x in ISO3166:
+        #     urls.append('https://www.xvideos.com/change-country/'+x.lower())     
+        # logger.info(urls)
+        urls = [
+            # 'https://www.xvideos.com/change-country/au',
+            'https://www.xvideos.com/change-country/us',
+            # 'https://www.xvideos.com/change-country/at',       
+        ]
         for url in urls:
             productResponse=scrapy.Request(url=url, callback=self.parse,meta=meta)
             # productResponse.meta['dont_cache'] = True
